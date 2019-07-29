311648db598e388b3e2ba562f87e42b8c4f675d2
Denisa Sufliarska <denisa.sufliarska@aas.local>
2019-07-29 11:20:41 +0200

    Initial commit

---
+++ src/test/java/Tests/SavingCalculatorTest.java
@@ -0,0 +1,51 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SavingCalculatorTest {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://localhost/savingscalculator.php");
+    }
+
+    @Test
+    public void itShouldDisplayTitle() {
+        Assert.assertEquals("Savings Calculator", driver.findElement(By.cssSelector("h1")).getText());
+    }
+
+    @Test
+    public void itShouldDisableApplyButtonOnPageOpen() {
+        Assert.assertFalse(driver.findElement(By.cssSelector("button.btn-block")).isEnabled());
+    }
+
+    @Test
+    public void itShouldEnableApplyButton(){
+        //1. vybrat fond
+        new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText("Hoggwart's Fund");
+        //2.zadat sumu
+        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys("1500");
+        //3.zadat pocet rokov
+        driver.findElement(By.id("yearsInput")).sendKeys("39");
+        //4.zadat email
+        driver.findElement(By.id("emailInput")).sendKeys("test@test.sk");
+        //5.overit button
+        Assert.assertTrue(driver.findElement(By.cssSelector("button.btn-block")).isEnabled());
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+}
